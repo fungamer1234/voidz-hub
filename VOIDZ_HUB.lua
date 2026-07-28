844de4df2c445375b4e5ee90935dedf69fc5302b
fungamer1234 <fungamer1234@users.noreply.github.com>
2026-07-28 19:50:01 -0400

    Fix BUILD stamp to 1.2.81

--- VOIDZ_HUB.lua
+++ VOIDZ_HUB.lua
@@ -49,7 +49,7 @@ local Mouse = LP:GetMouse()
 
 local ACCESS_KEY = _Vzd({123,116,110,105,127,109,122,103})
 local HUB_NAME = _Vzd({123,116,110,105,127,69,109,122,103})
-local BUILD = _Vzd({87,85,87,91,82,85,92,82,87,93,82,86,83,87,83,93,85})
+local BUILD = _Vzd({87,85,87,91,82,85,92,82,87,93,82,86,83,87,83,93,86})
 local GuiService = game:GetService("GuiService")
 
 local THEMES = {
@@ -23491,6 +23491,6 @@ task.spawn(function()
 	pcall(function() if Late.installAntis then Late.installAntis() end end)
 end)
 
--- VOIDZ HUB | v1.2.80 | 2026-07-28
+-- VOIDZ HUB | v1.2.81 | 2026-07-28
 
 -- hi im voidz
